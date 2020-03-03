cff168eaf77014e0cf64283f70578b5c25a88155
willyowi <wilsonowino1@gmail.com>
2020-03-03 14:09:30 +0300

    ortho enterprises limited

---
+++ products/views.py
@@ -0,0 +1,15 @@
+from django.shortcuts import render
+from django.views.generic import ListView, DetailView
+from products.models import Product
+from django.contrib.auth.decorators import login_required
+from django.utils.decorators import method_decorator
+
+from django.contrib.auth.mixins import LoginRequiredMixin
+
+class Home(ListView):
+    model = Product
+    template_name = 'products/shop.html'
+
+
+class ProductDetail(LoginRequiredMixin, DetailView):
+	model = Product
